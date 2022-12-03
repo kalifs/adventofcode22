84664a25a5008be8f7b00f6ddee4154f5dc66786
Arturs Meisters <arturs.meisters@gmail.com>
2022-12-03 05:59:04 +0000

    day 3 part 2

--- day3.rb
+++ day3.rb
@@ -12,11 +12,15 @@ end
 
 File.open(File.join(File.dirname(__FILE__),'day3.txt'), "r") do |f|
   sum = 0
+  parts = []
   f.each_line do |line|
     cLine = line.chomp
-    parts = [cLine.slice(0, cLine.size / 2), cLine.slice(cLine.size / 2, cLine.size)]
-    overlap = parts[0].split("") & parts[1].split("")
-    sum += value(overlap.first)
+    parts << cLine
+    if parts.size === 3
+      overlap = parts[0].split("") & parts[1].split("") & parts[2].split("")
+      sum += value(overlap.first)
+      parts = []
+    end
   end
   p sum
 end
