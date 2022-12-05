c1aca7a9b48232e47d86ad693a628abcd99d6a0b
Arturs Meisters <arturs.meisters@gmail.com>
2022-12-05 09:19:26 +0000

    day4 part2

--- day4.rb
+++ day4.rb
@@ -2,9 +2,9 @@ File.open(File.join(File.dirname(__FILE__),'day4.txt'), "r") do |f|
   covers = 0
   f.each_line do |line|
     ranges = line.chomp.split(",").map {|r| r.split("-").map(&:to_i) }
-    r1 = ranges[0][0]..ranges[0][1]
-    r2 = ranges[1][0]..ranges[1][1]
-    if r1.cover?(r2) || r2.cover?(r1)
+    r1 = (ranges[0][0]..ranges[0][1]).to_a
+    r2 = (ranges[1][0]..ranges[1][1]).to_a
+    if !(r1 & r2).empty?
       covers += 1
     end
   end
